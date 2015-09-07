793d78eed593be9c3b81c4501b45b5340d0d8207
Ranjeet Singh <ranjeetxsingh@gmail.com>
2015-09-07 11:36:33 +0530

    added irb inf-ruby minor mode for code eval on fly

--- lisp/init-ruby.el
+++ lisp/init-ruby.el
@@ -10,13 +10,21 @@
 
 ;;; Code:
 
-(battery:require-packages 'rspec-mode
+(battery:require-packages 'ruby-mode
+			  'inf-ruby
+			  'rspec-mode
 			  'ruby-tools
 			  'yaml-mode
 			  'ruby-electric)
 
 (eval-after-load 'rspec-mode
   '(rspec-install-snippets))
+
+(battery:after-initializing
+ (add-hook 'after-init-hook 'inf-ruby-switch-setup)
+ (autoload 'inf-ruby-minor-mode
+   "inf-ruby" "Run an inferior Ruby process" t)
+ (add-hook 'ruby-mode-hook 'inf-ruby-minor-mode))
 
 (provide 'init-ruby)
 
